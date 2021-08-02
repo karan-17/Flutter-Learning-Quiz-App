0d8156e730aa5561518db7ff3c50549fe6d8ef36
Karan Vaswani <karanvaswani2002@gmail.com>
2021-08-03 01:37:14 +0530

    Creating lists and maps

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;//pointer to the _answerQuestion function in main.dart
-
-  Answer(this.selectHandler);//pointer as an argument in the constructor
+  final String answerText;
+  Answer(this.selectHandler,this.answerText );//pointer as an argument in the constructor
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +11,7 @@ class Answer extends StatelessWidget {
       width: double.infinity,
       child:  ElevatedButton(
         onPressed: selectHandler,//with (), the constructor is executed as the program runs, but we want it onPressed
-        child: Text('Answer 1'),
+        child: Text(answerText),
       ),
     );
   }
