8ebd9164eed27dfdb8d6b1bc11b9d13772b4bb23
Karan Vaswani <karanvaswani2002@gmail.com>
2021-08-03 01:36:32 +0530

    Creating lists and maps

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,34 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _MyAppState(); //connected the 2 classes Myapp and MyAppState
   }
-}//private class conversion syntax "_MyAppState" leading underscore
+} //private class conversion syntax "_MyAppState" leading underscore
+
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      _questionIndex +=1;
+      _questionIndex += 1;
     });
     print(_questionIndex);
   }
+
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s my favourite colour?',
-      'What\'s  my favourite animal?'
+    const questions = [
+      {//Map is a collection of key-value combo
+        //Map is a separate class in dart, here shorthand used for map "{...}"
+        'questionText': 'What\'s my favourite colour?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      }, //Map
+      {
+        'questionText': 'What\'s  my favourite animal?',
+        'answers': ['Cat', 'Dog', 'Rabbit', 'Fish'],
+      }, //Map
+      {
+        'questionText': 'What\'s  my favourite dish?',
+        'answers': ['North Indian', 'South Indian', 'Continental', 'Chinese'],
+      }, //Map
     ];
     return MaterialApp(
       home: Scaffold(
@@ -39,16 +52,16 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]
+            Question(questions[_questionIndex]['questionText'],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-          ],
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()//executes a function on every element of the list
+          ],// ... take a list, pull all values out of list, add them to surrounding list as individual values
+          //adding values to the lists of list
         ),
       ),
       theme: ThemeData.dark(),
     );
   }
-  }
-
+}
