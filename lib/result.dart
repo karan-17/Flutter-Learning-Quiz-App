c46815a9e47d78c81a9a3437036241485979df4a
Karan Vaswani <karanvaswani2002@gmail.com>
2021-08-04 01:02:12 +0530

    Total Score Added

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  //adding getter
+  String get resultPhrase {
+    var resultText = 'You did it';
+    if(resultScore<= 8) {
+      resultText = 'You Scored $resultScore';
+    } else if(resultScore<=12){
+      resultText= 'You Scored $resultScore';
+    } else if(resultScore<=16){
+      resultText = 'You Scored $resultScore';
+    }else {
+      resultText = 'You Scored $resultScore';
+    }
+      return resultText;
+  }
+
+  Result(this.resultScore);
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You Did It!!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
