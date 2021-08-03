42ad845d7e3d677cd3ffc9f94b9cc5034cde9cf4
Karan Vaswani <karanvaswani2002@gmail.com>
2021-08-04 02:37:53 +0530

    Resetting the Quiz- Final

--- lib/result.dart
+++ lib/result.dart
@@ -2,30 +2,36 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
   //adding getter
   String get resultPhrase {
     var resultText = 'You did it';
-    if(resultScore<= 8) {
+    if (resultScore <= 8) {
       resultText = 'You Scored $resultScore';
-    } else if(resultScore<=12){
-      resultText= 'You Scored $resultScore';
-    } else if(resultScore<=16){
+    } else if (resultScore <= 12) {
       resultText = 'You Scored $resultScore';
-    }else {
+    } else if (resultScore <= 16) {
+      resultText = 'You Scored $resultScore';
+    } else {
       resultText = 'You Scored $resultScore';
     }
-      return resultText;
+    return resultText;
   }
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(onPressed: resetHandler, child: Text('Restart Quiz'))
+        ],
       ),
     );
   }
